082ce6a10ce1f1cc34420b1d714d9d1fbb946462
tetiana456 <60341879+tetiana456@users.noreply.github.com>
2020-01-27 12:20:55 +0200

    index1.php

---
+++ index.php
@@ -0,0 +1,71 @@
+﻿<html>
+	<head>
+		<title>Префікси системи Сі</title>
+		<style>
+		table{
+			width: 300px;
+			border: 5px solid gray;
+		}
+		td, th{
+			border: 1px solid black;
+			background-color: #FF6666;
+		}
+		th{
+			width: 100px;
+			background-color: #CC3333;
+		}
+		</style>
+	</head>
+	<body>
+		<table align="center">
+			<tr>
+				<th>Префікс</th>
+				<th>Позначення</th>
+				<th>Множник</th>
+			</tr>
+			<tr>
+				<td>кіло</td>
+				<td>к</td>
+				<td>10<sup>3</sup></td>
+			</tr>
+			<tr>
+				<td>мега</td>
+				<td>М</td>
+				<td>10</td>
+			</tr>
+			<tr>
+				<td>гіга</td>
+				<td>Г</td>
+				<td>10</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</table>
+	</body>
+</html>
